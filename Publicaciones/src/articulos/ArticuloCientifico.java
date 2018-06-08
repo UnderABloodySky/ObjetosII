51ad58f7f00c7a31b0c373232bc9345915d85e15
UnderABloodySky <valenzuelahoracioe@gmail.com>
2018-06-08 18:26:37 -0300

    Nuevo Constructor en ArticulosCientificos

--- Publicaciones/src/articulos/ArticuloCientifico.java
+++ Publicaciones/src/articulos/ArticuloCientifico.java
@@ -12,7 +12,7 @@ public class ArticuloCientifico implements Articulo{
 	private String lugar;
 	List<String> palabrasClaves;
 	
-		ArticuloCientifico(String _nombre, String autor, String _filiacion, String _tipo, String _lugar, List<String> _palabras){
+		public ArticuloCientifico(String _nombre, String autor, String _filiacion, String _tipo, String _lugar, List<String> _palabras){
 			nombre = _nombre;
 			autores = new ArrayList<String>(1);
 			autores.add(autor);
@@ -22,7 +22,7 @@ public class ArticuloCientifico implements Articulo{
 			palabrasClaves = _palabras;
 		}	
 
-		ArticuloCientifico(String _nombre, List<String> _autores, String _filiacion, String _tipo, String _lugar, List<String> _palabras){
+		public ArticuloCientifico(String _nombre, List<String> _autores, String _filiacion, String _tipo, String _lugar, List<String> _palabras){
 			nombre = _nombre;
 			autores = _autores;
 			filiacion = _filiacion;
@@ -34,9 +34,5 @@ public class ArticuloCientifico implements Articulo{
 		@Override
 		public boolean trataSobre(String interes) {
 			return palabrasClaves.contains(interes);
-		}	
-		
-		
-		
-		
+		}			
 }
